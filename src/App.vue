9da7db7797956cdfefdf5ec3d857cd391aa9f67d
Francesco <fra.boasso@gmail.com>
2022-03-14 16:36:56 +0100

    making new things

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+  <span class="title">{{ title }}</span>
+  <input class="inputText focus:outline-none focus:bg-white focus:border-purple-500" type="text" ref="name">
+  <button @click="handleClick" class="btn hover:bg-purple-400 focus:shadow-outline focus:outline-none">click me</button>
+  <Modal></Modal>
+</template> 
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'                           // import the component that we just made
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Modal },                                               // also register the component here
+  data() {
+    return {
+      title: "This is the title of the component"
+    }
+  },
+  methods: {
+    handleClick(){
+      console.log(this.$refs.name.value);      
+    }
   }
 }
 </script>
 
 <style>
+
+.title {
+  @apply border-b-2 block pb-2.5 text-4xl  
+}
+
+.inputText {
+  @apply appearance-none border-2 border-gray-200 rounded py-2 px-4 mt-2 text-gray-700 leading-tight
+}
+
+.btn {
+  @apply shadow bg-purple-500 text-white font-bold py-2 px-4 rounded ml-2
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Modal.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="backdrop">
+        <div class="modal">
+            <p>modal content</p>
+        </div>
+    </div>
+</template>
+
+
+<style>
+.modal{
+    @apply w-96 p-5 mt-44 mx-auto bg-white rounded-lg
+}
+
+.backdrop{        
+    @apply top-0 fixed bg-zinc-600/50 w-full h-full
+}
+</style>
