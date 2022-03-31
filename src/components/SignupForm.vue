bb3a7692fb8e2342725fc25a59dd39e61fb7d941
Francesco <fra.boasso@gmail.com>
2022-03-31 23:50:23 +0200

    Inizio Vue Form :O

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,12 @@
+<template>
+    <form>
+        <label class="email">Email</label>
+        <input type="email" class="input" required>
+    </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
